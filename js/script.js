8e48f796f7d0e6ff1d04b9ab04cd33f96a23e911
Jeremiah Garza <jag_tt@yahoo.com>
2019-10-21 08:15:01 -0500

    getRandomQuote function added

--- js/script.js
+++ js/script.js
@@ -5,27 +5,32 @@ project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
+const quotes = [
+  { quote: "abc", source: "", citation: "", year: "" },
+  { quote: "123", source: "" },
+  { quote: "asd", source: "" },
+  { quote: "zxc", source: "" },
+  { quote: "311", source: "" }
+];
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+console.log(quotes);
 
+let getRandomQuote = () => {
+  let ranQ = quotes[Math.floor(Math.random() * quotes.length)];
+  return [ranQ];
+};
 
-
+// function getRandomQuote() {
+//   let ranQ = quotes[Math.floor(Math.random() * quotes.length)];
+//   return [ranQ];
+// }
+console.log(getRandomQuote());
 
 /***
   Create the `getRandomQuote` function to:
    - Create a variable to store a random number 
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
-
-
-
 
 /***
   Create the `printQuote` function to: 
@@ -40,9 +45,6 @@ project 1 - A Random Quote Generator
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 
-
-
-
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
@@ -50,7 +52,8 @@ project 1 - A Random Quote Generator
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+document
+  .getElementById("loadQuote")
+  .addEventListener("click", printQuote, false);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
